af55a6e6403673f2f49e93be21432250ee9cd293
Claire <clairelporter26@gmail.com>
2020-02-16 16:07:16 +0000

    added first JS code

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,38 @@
 import '../styles/styles.css'
 
-if (module.hot){
+
+
+//mobile menu function//
+
+class MobileMenu {
+    constructor(){
+      this.menuIcon = document.querySelector(".site-header__menu-icon")
+      this.menuContent = document.querySelector(".site-header__menu-content")
+      this.events()
+    }
+
+    events() {
+        this.menuIcon.addEventListener("click", () => this.toggleTheMenu())
+    }
+
+    toggleTheMenu() {
+       this.menuContent.classList.toggle("site-header__menu-content--is-visible")
+    }
+}
+
+let mobileMenu = new MobileMenu();
+
+
+
+if (module.hot) {
     module.hot.accept()
 }
+
+
+
+
+
+
+
+
+
