423ab4a1a43d67c0342b0e10ca870c4935da9556
Claire <clairelporter26@gmail.com>
2020-02-21 17:09:32 +0000

    Completed desktop and mobile header styles

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -8,6 +8,7 @@ class MobileMenu {
     constructor(){
       this.menuIcon = document.querySelector(".site-header__menu-icon")
       this.menuContent = document.querySelector(".site-header__menu-content")
+      this.siteHeader = document.querySelector(".site-header")
       this.events()
     }
 
@@ -17,6 +18,8 @@ class MobileMenu {
 
     toggleTheMenu() {
        this.menuContent.classList.toggle("site-header__menu-content--is-visible")
+        this.siteHeader.classList.toggle("site-header--is-expanded")
+        this.menuIcon.classList.toggle("site-header__menu-icon--close-x")
     }
 }
 
